3bcfeea2685ae9ff31a06ca1fbc3073fdae4b706
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-13 16:47:39 +0600

    solve problem number 1

--- firstProblem.ts
+++ firstProblem.ts
@@ -1,2 +1,22 @@
-const test : string = "hello world" ;
-console.log(test);
+// -----------------Problem 1 ---------------
+
+function formatValue(data : string | number | boolean): string | number | boolean {
+if (typeof data === "string"){
+    return data.toUpperCase();
+}
+else if (typeof data === "number"){
+    return data * 10;
+}
+else if ( typeof data === "boolean"){
+    return !data;
+}
+
+throw new Error("Invalid type");
+
+}
+
+// -----------------Problem 2 ---------------
+
+
+
+
