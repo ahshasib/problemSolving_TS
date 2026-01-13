1b34298701e58aefa126aead4c67f211a4b1a88d
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-13 18:48:33 +0600

    solved problem 5

--- firstProblem.ts
+++ firstProblem.ts
@@ -67,3 +67,22 @@ return items.filter(item => item.rating >= 4);
 
 
 // -----------------Problem 5 -----------------
+
+type Data = {
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+
+function filterActiveUsers (allData: Data[]):Data[]{
+    return allData.filter(data => data.isActive === true);
+}
+
+const users = [
+    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+  ];
+  
+  console.log(filterActiveUsers(users));
