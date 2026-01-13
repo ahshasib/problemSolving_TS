4ad1e79702336c5b04473681d04fce96e896d7b5
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-13 18:26:55 +0600

    removed some input line

--- firstProblem.ts
+++ firstProblem.ts
@@ -56,10 +56,14 @@ function filterByRating (items:Item[]){
 return items.filter(item => item.rating >= 4);
 }
 
-const books = [
-    { title: 'Book A', rating: 4.5 },
-    { title: 'Book B', rating: 3.2 },
-    { title: 'Book C', rating: 5.0 },
-  ];
+// const books = [
+//     { title: 'Book A', rating: 4.5 },
+//     { title: 'Book B', rating: 3.2 },
+//     { title: 'Book C', rating: 5.0 },
+//   ];
   
-  console.log(filterByRating(books));
+//   console.log(filterByRating(books));
+
+
+
+// -----------------Problem 5 -----------------
