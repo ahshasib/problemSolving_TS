c7e9590c960607410fe8597da200f3570602f7f1
ahshasib <154332574+ahshasib@users.noreply.github.com>
2026-01-14 20:23:34 +0600

    removed console from problem 8

--- firstProblem.ts
+++ firstProblem.ts
@@ -184,12 +184,12 @@ interface Product {
   }
   
   
-  const products: Product[] = [
-    { name: 'Pen', price: 10, quantity: 2 },
-    { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
-    { name: 'Bag', price: 50, quantity: 1, discount: 20 },
-  ];
+//   const products: Product[] = [
+//     { name: 'Pen', price: 10, quantity: 2 },
+//     { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
+//     { name: 'Bag', price: 50, quantity: 1, discount: 20 },
+//   ];
   
   
-  console.log(calculateTotalPrice(products));
+//   console.log(calculateTotalPrice(products));
   
